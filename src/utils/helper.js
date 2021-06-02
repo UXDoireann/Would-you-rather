616073eea35fa5f059a0ac3e479c9277442fce7c
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-06-02 13:13:58 +0100

    Chore:Create helper function file

---
+++ src/utils/helper.js
@@ -0,0 +1,33 @@
+import{
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer
+} from '../_DATA.js'
+
+export function getInitialData(){
+    return Promise.all([
+        _getUsers(),
+        _getQuestions()
+    ]).then(([users, questions])=>({
+        users,
+        questions
+    }))
+}
+
+export function saveQuestion(question){
+    return _saveQuestion(question)
+}
+
+export function saveQuestionAnswer(answer){
+    return _saveQuestionAnswer(answer)
+}
+
+
+
+
+export function formatDate (timestamp) {
+    const d = new Date(timestamp)
+    const time = d.toLocaleTimeString('en-US')
+    return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+  }
