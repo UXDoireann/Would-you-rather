f1060a5b105a7607e48a4ccbecff660e1925c9e0
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-06-10 21:42:30 +0100

    Try:Unsuccessfully attempt at resizing avatars

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react' 
 import {connect} from 'react-redux' 
+import { makeStyles } from '@material-ui/core/styles';
 import { Avatar } from '@material-ui/core';
+
 
 
 class Question extends Component{
@@ -12,21 +14,50 @@ class Question extends Component{
 
         const { optionOne, optionTwo} = question
       
-
-      
+        const useStyles = makeStyles((theme) => ({
+          root: {
+            display: 'flex',
+            '& > *': {
+              margin: theme.spacing(1),
+            },
+          },
+          small: {
+            width: theme.spacing(3),
+            height: theme.spacing(3),
+          },
+          large: {
+            width: theme.spacing(7),
+            height: theme.spacing(7),
+          },
+        }));
+        
+        export default function ImageAvatars() {
+          const classes = useStyles(); 
+        
 
         return(
-            <div className ='question'>
-             <Avatar  alt={this.props.author} src={this.props.userPic}/>
-           
-             <h3>{this.props.author}</h3> 
-             <p>Would you rather...?</p> 
+            <div className ='question_card'>
+
+            <div className='user'>
+            <div className={classes.root}>
+                <Avatar  className='avatar' alt={this.props.author} src={this.props.userPic}/>
+           </div>
+               <h3>{this.props.author} asks if you'd rather...</h3> 
+           </div>
+
+            <div className='actual_question'>
              <p>{optionOne.text}</p>
-             <p>{optionTwo.text}</p>
+             <p>or</p>
+             <p>{optionTwo.text}?</p>
+            </div>
+            
+             <button className="view_poll">View Poll</button>
 </div>
         )
+        }
+      }
     }
-}
+
 
 function mapStateToProps({ questions, users}, {id}){
     const question = questions[id];
